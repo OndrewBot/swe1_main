66a6eb4314ecbd3ccadfa29cafc5dea6da1620f7
Andrew Bottom <bottoma@oregonstate.edu>
2024-08-14 09:22:33 -0400

    added fetch req for ind recipe view

--- src/pages/RecipesView.js
+++ src/pages/RecipesView.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import {React, useState} from 'react';
 import { useLocation } from 'react-router-dom';
 
 
 function RecipesView(){
-
+    const {recipe, setRecipe} = useState(null);
     const location = useLocation();
-    const recipe = location.state || { name: '', description: '', ingredients: [] };
-    console.log('Location State:', recipe);
-    if (!recipe) {
-        return <p>Loading...</p>;
-    }
+    const id = location.state;
+    
+    useEffect(() => {
+        (async () => {
+            try {
+                const response = await fetch(`https://cs361-recipes.onrender.com/recipes/${id}`, {
+                    method: 'GET'
+                });
+                if (response.ok) {
+                    const content = await response.json();
+                    const sorted_ingredients = [...content.ingredients].sort((a, b) => a.ingredient.localeCompare(b.ingredient));
+                    setRecipe({...content, ingredients: sorted_ingredients});
+                } else {
+                    console.error('Failed to fetch recipes:', response.status);
+                }
+            } catch (error) {
+                console.error('Error fetching recipes:', error);
+            }
+        })();
+    }, []);
+
     return (
         <>
             <h1 class="display-5">{recipe.name}</h1>
