ae71195e4efe27b947cd5895ee5cc40b4657e821
Andrew Bottom <bottoma@oregonstate.edu>
2024-08-14 07:37:47 -0400

    fixed err

--- src/pages/RecipesView.js
+++ src/pages/RecipesView.js
@@ -11,32 +11,32 @@ function RecipesView(){
     }
     return (
         <>
-        <h1 class="display-5">{recipe.name}</h1>
-        <p class="lead">{recipe.description}</p>
-        <div class="card d-inline-flex align-items-center" style={{width: 60 + '%'}}>
-            <div class="col-11 mt-1 ms-2 me-3 text-start">
-                <div class="input-group mb-3">
-                    <button class="btn btn-primary" type="button">Edit</button>
+            <h1 class="display-5">{recipe.name}</h1>
+            <p class="lead">{recipe.description}</p>
+            <div class="card d-inline-flex align-items-center" style={{width: 60 + '%'}}>
+                <div class="col-11 mt-1 ms-2 me-3 text-start">
+                    <div class="input-group mb-3">
+                        <button class="btn btn-primary" type="button">Edit</button>
+                    </div>
+                    
+                </div>  
+                <div class="card-body" style={{width: 90 + '%'}}>
+                    <table class="table table-hover">
+                        <thead>
+                            <tr>
+                                <th scope="col">Ingredients</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                        {recipe.ingredients.map((item, index) => {
+                            return <tr scope="row" key={index}>
+                                <td>{item.amount} {item.unit} of {item.ingredient}</td>
+                            </tr>
+                        })}
+                        </tbody>
+                    </table>
                 </div>
-                
-            </div>  
-            <div class="card-body" style={{width: 90 + '%'}}>
-                <table class="table table-hover">
-                    <thead>
-                        <tr>
-                            <th scope="col">Ingredients</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                    {recipe.ingredients.map((item, index) => {
-                        return <tr scope="row" key={index}>
-                            <td>{item.amount} {item.unit} of {item.ingredient}</td>
-                        </tr>
-                    })}
-                    </tbody>
-                </table>
             </div>
-        </div>
         </>
     );
 }
