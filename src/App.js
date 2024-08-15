eee52e17201d5c7b3050ab91c0d1962368283186
Andrew Bottom <bottoma@oregonstate.edu>
2024-08-15 18:38:17 -0400

    help

--- src/App.js
+++ src/App.js
@@ -7,7 +7,6 @@ import Recipes from './pages/Recipes';
 import RecipesNew from './pages/RecipesNew';
 import RecipesEdit from './pages/RecipesEdit.js';
 import RecipesView from './pages/RecipesView';
-// import ShoppingList from './pages/ShoppingList';
 import Ingredients from './pages/Ingredients';
 import KitchenTimer from './pages/KitchenTimer';
 import About from './pages/About';
@@ -24,7 +23,6 @@ function App() {
             <Route path="/recipes/new" element={<RecipesNew />}/>
             <Route path="/recipes/edit/:id" element={<RecipesEdit />}/>
             <Route path="/recipes/:id" element={<RecipesView />}/>
-            {/* <Route path="/shoppinglist" element={<ShoppingList />}/> */}
             <Route path="/ingredients" element={<Ingredients />}/>
             <Route path="/timer" element={<KitchenTimer />}/>
             <Route path="/about" element={<About />}/>
